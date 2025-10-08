e77e12d212b85e0a8744e33ecefcaaad0ac737a0
方兆阳 <fzhaoyang2010@outlook.com>
2025-10-08 10:43:54 +0800

    Delete src directory

--- src/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Core subsystem exports."""
-
-from .digital_life import TrueDigitalLife
-from .environment import DigitalEnvironment
-from .life_states import LifeState
-
-__all__ = ["TrueDigitalLife", "DigitalEnvironment", "LifeState"]
